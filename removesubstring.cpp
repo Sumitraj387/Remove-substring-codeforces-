0a8de750d5942d2cc894778381a632c0cd046f17
Sumit Raj <43118760+Sumitraj387@users.noreply.github.com>
2019-10-08 20:34:26 +0530

    Create removesubstring.cpp

---
+++ removesubstring.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long int
+#define IOS ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define pb push_back
+#define pop pop_back
+#define str string
+#define endl "\n"
+#define rtn return(0)
+int check(str s1, str s2)
+{
+    int n1=s1.size(),n2=s2.size(),i,j,c=0,a=0;
+    if(n1<n2)
+        return(0);
+    else if(n1==n2)
+    {
+        if(s1==s2)
+            return(1);
+        else
+            return(0);
+    }
+    else
+    {
+        for(i=0;i<n1;i++)
+        {
+            if(s1[i]==s2[a])
+            {
+                c++;
+                a++;
+                if(c==n2)
+                    return(1);
+                if(a>=n2)
+                    return(0);
+            }
+        }
+    }
+    return(0);
+}
+main()
+{
+    IOS
+    str s,t;
+    cin>>s>>t;
+    int l1=s.size(),max1=0;
+    int l=1,u=l1,mid,i;
+    while(l<=u)
+    {
+        mid=(l+u)/2;
+        int a=max1;
+        for(i=0;i<=(l1-mid);i++)
+        {
+            str s1=s;
+            s1.erase(s1.begin()+i,s1.begin()+i+mid);
+            if(check(s1,t))
+            {
+                if(max1<mid)
+                    max1=mid;
+                break;
+            }
+        }
+        if(max1==a)
+            u=mid-1;
+        else
+            l=mid+1;
+ 
+    }
+    cout<<max1;
+ 
+}
